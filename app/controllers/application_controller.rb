41fe62f8496ab2689f0ec7183f6f029e59025d3b
krallm <krallm@southwestern.edu>
2015-02-17 19:01:12 -0600

    Force sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Make users sign in before they can do anything
+  before_action :authenticate_user!
 end
