3f2daedf30bdb697c023a78a4f7a02a79f844d27
krallm <krallm@southwestern.edu>
2015-02-17 19:12:10 -0600

    Simple WelcomeController tests

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index should redirect to login page if user not signed in" do
+    get :index
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "index shouldn't redirect if signed in" do
+    user = users :one
+    sign_in user
+    get :index
+    assert_response :success
+  end
 end
